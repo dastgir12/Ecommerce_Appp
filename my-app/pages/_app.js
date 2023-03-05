a3e9c94aadf4fa1b7fa6dbb4647230abc48bd268
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-05 17:17:55 +0500

    init

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,10 +1,16 @@
-import '../styles/globals.css'
-import { Layout } from '../components'
+import "../styles/globals.css";
+import { Layout } from "../components";
+import { Toaster } from "react-hot-toast";
+import { StateContext } from "../context/StateContext";
 function MyApp({ Component, pageProps }) {
-  return(
-  <Layout>
-  <Component {...pageProps} />
-  </Layout>
-)}
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
+}   
 
-export default MyApp
+export default MyApp;
--- my-app/pages/product/[slug].js
+++ my-app/pages/product/[slug].js
@@ -3,12 +3,19 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
-
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
+
   return (
     <div>
       <div className="product-detail-container">
@@ -48,13 +55,13 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick=''><AiOutlineMinus /></span>
-              <span className="num"></span>
-              <span className="plus" onClick=''><AiOutlinePlus /></span>
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick=''>Add to Cart</button>
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
             <button type="button" className="buy-now" onClick=''>Buy Now</button>
           </div>
         </div>
